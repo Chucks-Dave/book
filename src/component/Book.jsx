cd3cc0200adf8b0af56cc04156cfede82c7c7d05
okechukwu david <david@koonage.com>
2024-12-13 06:42:13 +0100

    added framer motion

--- src/component/Book.jsx
+++ src/component/Book.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const Book = () => {
   const [currentTime, setCurrentTime] = useState(null);
@@ -20,6 +21,19 @@ const Book = () => {
   if (!currentTime) return null;
 
   const { fullYear } = formatTime(currentTime);
+  const words = " THANK YOU!";
+  const letters = words.split("");
+  const pullupVariant = {
+    initial: { y: 100, opacity: 0 },
+    animate: (i) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: i * 0.05, // Delay each letter's animation by 0.05 seconds
+      },
+    }),
+  };
+
   return (
     <div className="custom-scrollbar ">
       <div
@@ -29,14 +43,25 @@ const Book = () => {
         // }}
       >
         {" "}
-        <p className="text-center font-black text-[150px] text-purple">
-          {" "}
-          THANK YOU!
-        </p>
+        <div className="flex justify-center items-center">
+          {letters.map((letter, i) => (
+            <motion.h1
+              key={i}
+              variants={pullupVariant}
+              initial="initial"
+              animate="animate"
+              custom={i}
+              className="text-center font-outfit text-purple lg:text-[150px]   font-black tracking-[-0.02em] drop-shadow-sm max-md:text-6xl md:leading-[5rem]"
+            >
+              {letter === " " ? <span>&nbsp;</span> : letter}
+            </motion.h1>
+          ))}
+             
+        </div>
       </div>
 
-      <div className="relative top-[220px]">
-        <div className="px-8   bg-[#F7F7F7] w-full space-y-7">
+      <div className="relative max-md:top-[480px] lg:top-[350px]">
+        <div className="px-8    w-full space-y-7">
           <div className=" flex flex-col justify-center items-center gap-3">
             <div className="text-black max-md:text-center">
               &copy; {fullYear} All rights Reserved from Rayzer Business
